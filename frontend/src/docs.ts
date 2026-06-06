7441030f3f0bace721ee3a83c9b78348b4a32e57
Eldyn <oliva.alessandro.b@gmail.com>
2026-06-06 15:54:18 +0200

    docs(): add entrypoint for documentation

---
+++ frontend/src/docs.ts
@@ -0,0 +1,9 @@
+export * from "./lib/stores/ws.svelte";
+export * from "./lib/stores/auth.svelte";
+export * from "./lib/stores/game.svelte";
+export * from "./lib/stores/lobby.svelte";
+export * from "./lib/stores/navigation.svelte";
+export * from "./lib/stores/toast.svelte";
+
+export * from "./lib/utils/errors";
+export * from "./lib/utils/validation";
