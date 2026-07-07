Eldyn/uni-uno

feat: mock for responsive lobby browse ui

frontend/src/lib/components/game/GameCard.svelte
@@ -56,7 +56,8 @@
 	}
 
 	const imgSrc = $derived(getCardImage(card.value));
-	const tint = $derived(card.value !== "colorswitch");
+	const valueTint = $derived(card.value !== "colorswitch");
+	const borderTint = $derived(card.value !== "colorswitch");
 	const slotAttach = (node: Element) => attach?.(node);
 </script>
 
@@ -84,7 +85,7 @@
 			<img src="assets/cards/back.png" alt="" class="layer-bg" />
 		{:else}
 			<img src="assets/cards/background.png" alt="" class="layer-bg" />
-			{#if imgSrc && tint}
+			{#if imgSrc && valueTint}
 				<div class="layer-mask">
 					<TintedSprite src={imgSrc} color="var(--card-color)" fit="100% 100%" />
 				</div>
@@ -92,9 +93,13 @@
 				<img src={imgSrc} alt="" class="layer-bg" />
 			{/if}
 
-			<div class="layer-mask">
-				<TintedSprite src="/assets/cards/border.png" color="var(--card-color)" fit="100% 100%" />
-			</div>
+			{#if borderTint}
+				<div class="layer-mask">
+					<TintedSprite src="/assets/cards/border.png" color="var(--card-color)" fit="100% 100%" />
+				</div>
+			{:else}
+				<img src="/assets/cards/border.png" alt="" class="layer-bg" />
+			{/if}
 		{/if}
 	</div>
 </div>

Eldyn/uni

frontend/src/lib/components/game/GameCard.svelte
@@ -50,8 +50,9 @@
 	}
 
 	const imgSrc = $derived(getCardImage(card.value));
-	const tint = $derived(card.value !== "jolly");
 	const cardColor = $derived(COLOR_MAP[card.type] ?? "inherit");
+	const valueTint = $derived(card.value !== "jolly");
+	const borderTint = $derived(card.value !== "jolly");
 	const slotAttach = (node: Element) => attach?.(node);
 </script>
 
@@ -79,7 +80,7 @@
 			<img src="assets/cards/back.png" alt="" class="layer-bg" />
 		{:else}
 			<img src="assets/cards/background.png" alt="" class="layer-bg" />
-			{#if imgSrc && tint}
+			{#if imgSrc && valueTint}
 				<div class="layer-mask">
 					<TintedSprite src={imgSrc} color="var(--card-color)" fit="100% 100%" />
 				</div>
@@ -87,9 +88,13 @@
 				<img src={imgSrc} alt="" class="layer-bg" />
 			{/if}
 
-			<div class="layer-mask">
-				<TintedSprite src="/assets/cards/border.png" color="var(--card-color)" fit="100% 100%" />
-			</div>
+			{#if borderTint}
+				<div class="layer-mask">
+					<TintedSprite src="/assets/cards/border.png" color="var(--card-color)" fit="100% 100%" />
+				</div>
+			{:else}
+				<img src="/assets/cards/border.png" alt="" class="layer-bg" />
+			{/if}
 		{/if}
 	</div>
 </div>